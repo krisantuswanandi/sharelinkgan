f7c87995dc9d51ae1f282fbbecd8424905345a8a
Krisantus Wanandi <krisantus.wanandi@gmail.com>
2023-07-22 22:57:56 +0700

    open shortened url

--- database/schema.ts
+++ database/schema.ts
@@ -1,3 +1,4 @@
+import { InferModel } from "drizzle-orm";
 import { mysqlTable, serial, text, varchar } from "drizzle-orm/mysql-core";
 
 export const url = mysqlTable("url", {
@@ -5,3 +6,5 @@ export const url = mysqlTable("url", {
   hash: varchar("hash", { length: 10 }).unique(),
   original: text("original"),
 });
+
+export type Url = InferModel<typeof url, "select">;
---
+++ server/routes/s/[hash].ts
@@ -0,0 +1,27 @@
+import { eq } from "drizzle-orm";
+import { db, schema } from "@/database";
+import { Url } from "../../../database/schema";
+
+export default defineEventHandler(async (event) => {
+  const hash = event.context.params?.hash;
+
+  if (!hash) {
+    return sendRedirect(event, "/");
+  }
+
+  try {
+    const url: Url[] = await db
+      .select()
+      .from(schema.url)
+      .where(eq(schema.url.hash, hash));
+
+    if (url.length !== 1) {
+      return sendRedirect(event, "/");
+    }
+
+    return sendRedirect(event, `/#${url[0].original}`);
+  } catch (error) {
+    console.error(error);
+    return sendRedirect(event, "/");
+  }
+});
