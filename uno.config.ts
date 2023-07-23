978ffdd832061745773761f64cb4487949cd4c51
Krisantus Wanandi <krisantus.wanandi@gmail.com>
2023-07-24 00:14:16 +0700

    add uno web fonts

--- uno.config.ts
+++ uno.config.ts
@@ -1,5 +1,17 @@
-import { defineConfig, presetUno } from "unocss";
+import { defineConfig, presetUno, presetWebFonts } from "unocss";
 
 export default defineConfig({
-  presets: [presetUno()],
+  presets: [
+    presetUno(),
+    presetWebFonts({
+      provider: "google",
+      fonts: {
+        sans: {
+          name: "Inter",
+          weights: [400, 700],
+        },
+        mono: "Fira Code",
+      },
+    }),
+  ],
 });
